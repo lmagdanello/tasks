5e019ce3682ecd37be1853d2c084de2115a7e44e
Leonardo Magdanello <lmagdanello40@gmail.com>
2021-11-03 00:22:35 -0300

    tasks: first commit

---
+++ cmd/root.go
@@ -0,0 +1,8 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var RootCmd = &cobra.Command{
+	Use: "tasks",
+	Short: "tasks is a CLI TODO list manager",
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/lmagdanello/tasks/cmd"
+
+func main() {
+	cmd.RootCmd.Execute()
+}
