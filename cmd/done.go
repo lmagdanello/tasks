ab3a8c3c896ed139fd37e69ad4330ccd7ef4d088
Leonardo Magdanello <lmagdanello40@gmail.com>
2021-11-04 09:15:08 -0300

    tasks: test done argument conversion

--- cmd/done.go
+++ cmd/done.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +11,16 @@ var doneCmd = &cobra.Command{
 	Use:   "done",
 	Short: "Marks a task as complete",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("done called")
+		var idList []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				idList = append(idList, id)
+			}
+		}
+		fmt.Println(idList)
 	},
 }
 
