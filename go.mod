53b1e02cf9e3249b1d304dc9c16f74f73769e83c
Leonardo Magdanello <lmagdanello40@gmail.com>
2021-11-04 10:34:57 -0300

    db: Init db

--- go.mod
+++ go.mod
@@ -2,8 +2,12 @@ module github.com/lmagdanello/tasks
 
 go 1.17
 
+require github.com/spf13/cobra v1.2.1
+
 require (
+	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/spf13/cobra v1.2.1
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import ("github.com/lmagdanello/tasks/cmd")
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/lmagdanello/tasks/cmd"
+	"github.com/lmagdanello/tasks/db"
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		log.Println(err.Error())
+		os.Exit(1)
+	}
 }
