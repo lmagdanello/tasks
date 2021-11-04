a222ba2f2067572dc8c8dbe0cb97069e5bacc086
Leonardo Magdanello <lmagdanello40@gmail.com>
2021-11-04 11:06:05 -0300

    db: Add func to list tasks, remove and create new tasks

--- cmd/list.go
+++ cmd/list.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/lmagdanello/tasks/db"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +13,20 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			log.Println("Something went wrong: ", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks!")
+			return
+		}
+
+		fmt.Println("List tasks:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
--- db/tasks.go
+++ db/tasks.go
@@ -8,6 +8,7 @@ import (
 )
 
 var taskBucket = []byte("tasks")
+
 var db *bolt.DB
 
 type Task struct {
@@ -43,6 +44,34 @@ func CreateTask(task string) (int, error) {
 	}
 
 	return id, nil
+}
+
+func AllTasks() ([]Task, error) {
+	var tasks []Task
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func DeleteTask(key int) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		return b.Delete(itob(key))
+	})
 }
 
 func itob(v int) []byte {
